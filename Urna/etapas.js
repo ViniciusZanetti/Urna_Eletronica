7d45e67b75feb5206c85a4d806bd7b84cdbdca66
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-09-16 16:56:49 -0300

    metade da página pronta só falta o js

---
+++ Urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+{
+            titulo: 'Vereador',
+            numeros: 5,
+            candidatos:[{
+                numero: 38111,
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url: '00-original.jpg', legenda: 'Vereador', small: false}
+                ]
+        },
+        {
+            numero: 77222,
+            name: 'Beltrano da silva',
+            partido: 'DEFG',
+            fotos:[
+                {url:'43-original.jpg', legenda: 'Vereador'}
+            ]
+        },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: '77-original.jpg', legenda: 'Prefeito'},
+                    {url: '84-original.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url: '97-original.jpg', legenda: 'Prefeito'},
+                    {url: '99-original.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    } 
+
+]
