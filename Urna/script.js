8bfe0d19d6c88cdb2add7c494a89c131a7a5553b
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-09-20 12:37:01 -0300

    urna eletrônica finalizada

--- Urna/script.js
+++ Urna/script.js
@@ -7,11 +7,15 @@ let numeros = document.querySelector('.d-1-3')
 
 let etapaAtual = 0
 let numero = ''
+let votoBranco = false
+let votos = []
 
 function comecarEtapa(){
     let etapa = etapas[etapaAtual]
 
     numeroHtml = ''
+    numero = ''
+    votoBranco = false
 
     for(let i = 0; i < etapa.numeros; i++){
         if(i === 0){
@@ -46,10 +50,14 @@ function atualizaInterface(){
 
         let fotosHtml = ''
         for(let i in candidato.fotos){
-            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`
+            fotosHtml += `<div class="d-1-image "><img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`
         }
 
         lateral.innerHTML = fotosHtml
+    }else{
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        descricao.innerHTML= '<div class ="aviso--grande pisca">VOTO NULO</div>'
     }
 
     console.log("Candidato", candidato)
@@ -72,15 +80,54 @@ function clicou(n){
 }
 
 function branco(){
-    alert("Clicou em BRANCO")
+    if(numero === ''){
+        numero = ''
+        votoBranco = true
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        numeros.innerHTML = ''
+        descricao.innerHTML= '<div class ="aviso--grande pisca">VOTO EM BRANCO</div>'
+        lateral.innerHTML = ''
+
+    }else{
+        alert("Para votar em BRANCO, não pode ter digitado nenhum número!")
+    }
 }
 
 function corrige(){
-    alert("Clicou em CORRIGE")
+    comecarEtapa()
 }
 
 function confirma(){
-    alert("Clicou em CONFIRMA")
+
+    let etapa = etapas[etapaAtual]
+
+    let votoConfirmado = false
+
+    if(votoBranco === true){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+        console.log("Confirmando como branco...")
+    }else if(numero.length === etapa.numeros){
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    if(votoConfirmado){
+        etapaAtual++
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa()
+        }else{
+            document.querySelector('.tela').innerHTML = '<div class ="aviso--gigante pisca">FIM!</div>'
+            console.log(votos)
+        }
+    }
 }
 
 comecarEtapa()
